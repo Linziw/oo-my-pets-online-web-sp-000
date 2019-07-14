b6518c53f84d1070d21c2bfad3a0e6db2b8fa9c1
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-14 04:51:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ def sell_pets
 end
 
 def list_pets
-  "I have #{@pets[:fishes].length} fish,  dog(s), and #{} cat(s)."
+  "I have #{@pets[:fishes].length} fish, #{@pets[:fishes].length} dog(s), and  cat(s)."
 end
   
 
