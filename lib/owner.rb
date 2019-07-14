8117a5627b12d07edbf0338e3aa720bed9fa8e28
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-14 04:32:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+
+attr_accessor :all, :name, :pets, :fish,
+attr_reader :species
+
+@@all = []
+
+def initialize(species)
+ @species = species
+ @@all << self
+ @pets = {:fishes => [], :dogs => [], :cats=> []}
+ 
+  
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.length
+end
+
+def self.reset_all
+  @@all = []
+end
+
+def say_species
+ "I am a #{self.species}."
+end
+
 end
